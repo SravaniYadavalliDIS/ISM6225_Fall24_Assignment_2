4c884a88712741eaf35d30dbdb60c05d2adfc268
SreddyNimma <sandeepreddynimma@usf.edu>
2024-10-20 22:55:23 -0400

    solutions for code

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -63,8 +63,41 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new List<int>(); // Placeholder
+                if(nums.Length == 0)
+                {
+                    return new List<int>(); 
+                } else
+                {
+                  
+                int n = nums.Length;
+                HashSet<int> numSet = new HashSet<int>();
+
+                // Add all numbers in the array to the HashSet
+                foreach (int num in nums)
+                {
+                    if (num > 0 && num <= n)
+                    {
+                        numSet.Add(num);
+                    }
+                }
+
+                List<int> missingNumbers = new List<int>();
+
+                // Find the missing numbers by checking against the expected range
+                for (int i = 1; i <= n; i++)
+                {
+                    if (!numSet.Contains(i))
+                    {
+                        missingNumbers.Add(i);
+                    }
+                }
+
+                return missingNumbers; // Return the list of missing numbers
             }
+
+    }
+
+
             catch (Exception)
             {
                 throw;
@@ -77,7 +110,33 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new int[0]; // Placeholder
+                if(nums.Length==0)
+                {
+                    return new int[0];
+                }
+                else
+                {
+                    List<int> evens = new List<int>(); // List to hold even numbers
+                    List<int> odds = new List<int>();   // List to hold odd numbers
+
+                    // Traverse the original list and separate evens and odds
+                    foreach (var num in nums)
+                    {
+                        if (num % 2 == 0)
+                        {
+                            evens.Add(num); // Add even number to the evens list
+                        }
+                        else
+                        {
+                            odds.Add(num); // Add odd number to the odds list
+                        }
+                    }
+
+                    // Combine the even and odd lists
+                    evens.AddRange(odds);
+
+                    return evens.ToArray(); // Return the combined list
+                }
             }
             catch (Exception)
             {
@@ -91,7 +150,29 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new int[0]; // Placeholder
+                if (nums.Length == 0)
+                {
+                    return new int[0];
+                }
+                else
+                {
+                    List<int> result = new List<int>(); 
+                    // Iterate through each element in the array
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        // Check each subsequent element
+                        for (int j = i + 1; j < nums.Length; j++)
+                        {
+                            // Check if the current pair adds up to the target
+                            if (nums[i] + nums[j] == target)
+                            {
+                                result.Add(i);
+                                result.Add(j);
+                            }
+                        }
+                    }
+                    return result.ToArray();
+                }
             }
             catch (Exception)
             {
@@ -105,7 +186,18 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return 0; // Placeholder
+                //check if array length is less than 3 or not
+                if(nums.Length<3)
+                {
+                    return 0;
+                } else
+                {
+                    int len = nums.Length;
+                    Array.Sort(nums);
+                    int product1 = nums[len - 1] * nums[len - 2] * nums[len - 3]; // Three largest numbers
+                    int product2 = nums[0] * nums[1] * nums[len - 1];//Three smallest numbers
+                    return Math.Max(product1, product2);
+                }
             }
             catch (Exception)
             {
@@ -119,7 +211,17 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return "101010"; // Placeholder
+                string binNumber = "";
+                int rem;
+                while(decimalNumber>0)
+                {
+                    rem = decimalNumber % 2;
+                    //Adding the remainder to the beginning of the binaryNumber string
+                    binNumber = rem.ToString() + binNumber;
+                    //update the quotient by dividing it by 2.
+                    decimalNumber = decimalNumber / 2;
+                }
+                return binNumber;
             }
             catch (Exception)
             {
@@ -133,7 +235,18 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return 0; // Placeholder
+                //check the length of the array
+                if (nums.Length == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    //sorting the numbers array 
+                    Array.Sort(nums);
+                    //get the element from first index since it is sorted array in aescending order
+                    return nums[0];
+                }
             }
             catch (Exception)
             {
@@ -147,7 +260,26 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return false; // Placeholder
+                    int rem = 0, rev = 0,temp=x;
+                //It will iterate the loop until x>0
+                    while (x > 0)
+                    {
+                        //storing the last digit of the number by using remainder
+                        rem = x % 10;
+                    // It multiplies the current value of rev by 10  and adds the last digit 
+                    rev = rev * 10 + rem;
+                       // Remove the last digit by performing integer division
+                        x = x / 10;
+                    }
+                    //compare the original value with the value stored in rev
+                    if (rev == temp)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
             }
             catch (Exception)
             {
@@ -161,7 +293,20 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return 0; // Placeholder
+                // In this i have used the recursive function 
+                if(n<0)
+                {
+                    return -1;
+                } else if(n==0)
+                {
+                   return 0;
+                } else if(n==1)
+                {
+                    return 1;
+                } else
+                {
+                    return Fibonacci(n - 1) + Fibonacci(n - 2);
+                }
             }
             catch (Exception)
             {
